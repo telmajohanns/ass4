48e660265c3ba66fbd96b27f19f08add5d459cf5
telmajohanns <tej2@hi.is>
2024-02-15 22:51:51 +0000

    Adding the next prime number

--- src/main/java/is/hi/tej2/hbv202g/ass4/App.java
+++ src/main/java/is/hi/tej2/hbv202g/ass4/App.java
@@ -1,13 +1,15 @@
 package is.hi.tej2.hbv202g.ass4;
 
+import org.apache.commons.numbers.primes.Primes;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int prime = 123456789;
+        int next = Primes.nextPrime(prime);
+        System.out.println(next);
     }
 }
